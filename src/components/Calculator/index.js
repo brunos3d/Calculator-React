08925823330e93fc5b60281ebe3985135fa77120
Bruno Silva <bruno3dcontato@gmail.com>
2019-12-22 14:45:39 -0300

    Update index.js

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Container } from "./styles";
 
@@ -6,21 +6,56 @@ import Button from "../Button";
 import Display from "../Display";
 
 export default function Calculator() {
+    const initialState = {
+        displayValue: "0",
+        clearDisplay: false,
+        operation: null,
+        values: [0, 0],
+        current: 0
+    };
+
+    const [currentState, setCurrentState] = useState(initialState);
+
     function clearMemory() {
-        console.log("limpar");
+        setCurrentState(initialState);
     }
 
     function setOperation(operation) {
         console.log(operation);
     }
 
-    function addDigit(number) {
-        console.log(number);
+    function addDigit(n) {
+        if (n === "." && currentState.displayValue.includes(".")) {
+            return;
+        }
+
+        const clearDisplay = currentState.displayValue === "0" || currentState.clearDisplay;
+        const currentValue = clearDisplay ? "" : currentState.displayValue;
+        const displayValue = currentValue + n;
+
+        let newState = { ...currentState };
+
+        newState.displayValue = displayValue;
+        newState.clearDisplay = false;
+
+        setCurrentState(newState);
+
+        if (n !== ".") {
+            const i = newState.current;
+            const newValue = parseFloat(displayValue);
+            const values = [...newState.values];
+            values[i] = newValue;
+
+            newState = { ...newState };
+            newState.values = values;
+
+            setCurrentState(newState);
+        }
     }
 
     return (
         <Container>
-            <Display value={100} />
+            <Display value={currentState.displayValue} />
             <Button label="AC" click={clearMemory} triple />
             <Button label="/" click={setOperation} operation />
             <Button label="7" click={addDigit} />
