e0b1c9f3ac42af59b61c34ed34112c6fd46963ce
Bruno Silva <bruno3dcontato@gmail.com>
2019-12-22 13:12:48 -0300

    Updates

---
+++ precache-manifest.5e1bc55826a34a87115b641d78494994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f68c9b866ec88c67a99ae4ab70220b",
+    "url": "/BrunoS3D/Calculator-React/index.html"
+  },
+  {
+    "revision": "feb8e23076844342abea",
+    "url": "/BrunoS3D/Calculator-React/static/css/main.f4cba712.chunk.css"
+  },
+  {
+    "revision": "e44c3ef2f18ec6b2c107",
+    "url": "/BrunoS3D/Calculator-React/static/js/2.e3c20fd3.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/BrunoS3D/Calculator-React/static/js/2.e3c20fd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "feb8e23076844342abea",
+    "url": "/BrunoS3D/Calculator-React/static/js/main.276e5c2c.chunk.js"
+  },
+  {
+    "revision": "d5ce30a0600a72e342b7",
+    "url": "/BrunoS3D/Calculator-React/static/js/runtime-main.95bb3e3d.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/BrunoS3D/Calculator-React/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
